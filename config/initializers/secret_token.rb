16372db497be5d3db49c8805d8dbcbd095dc2873
Katiusza <ms.ksiwek@gmail.com>
2014-01-19 12:01:45 -0600

    Initialize Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TutApp::Application.config.secret_key_base = '902f91da7f17fa70c6aa91f16e490302a8b12d43e1689f0cfadb7f36edc4bc2a376c3bbc82154223107eec6928139872be13adce1dabc939248c0c7aab6f0488'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TutApp::Application.config.session_store :cookie_store, key: '_tut_app_session'
